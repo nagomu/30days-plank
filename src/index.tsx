a84e8ced785607dcb5bfba48cabdc73874eed0dd
Kaoru Wada <hello@nagomu.me>
2019-10-18 17:31:24 +0900

    Provide store

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
 import App from '~/components/App';
+import { store } from '~/store';
 
-ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root') as HTMLElement,
+);

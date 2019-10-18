8b66aa4a648573ac76659e8a8e8862424ac9cf69
Kaoru Wada <hello@nagomu.me>
2019-10-18 16:07:54 +0900

    Add withProvider util

--- src/utils/testHelpers.tsx
+++ src/utils/testHelpers.tsx
@@ -1,3 +1,23 @@
+import { mount, ReactWrapper } from 'enzyme';
+import * as React from 'react';
+import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 
 export const mockStore = configureStore([]);
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+type WithProviderParam = {
+  Component: React.FC;
+  props?: any;
+  store?: any;
+};
+/* eslint-enable */
+
+export const withProvider = (params: WithProviderParam): ReactWrapper => {
+  const { Component, props, store } = params;
+  return mount(
+    <Provider store={store || mockStore()}>
+      <Component {...props} />
+    </Provider>,
+  );
+};

62baacccc33bf49f85d691cb3dbb5407c9edc038
Kaoru Wada <hello@nagomu.me>
2019-10-23 22:41:09 +0900

    Fix lint

--- src/utils/testHelpers.tsx
+++ src/utils/testHelpers.tsx
@@ -1,8 +1,8 @@
 import { mount, ReactWrapper } from 'enzyme';
 import * as React from 'react';
 import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store';
 import { BrowserRouter } from 'react-router-dom';
+import configureStore from 'redux-mock-store';
 
 export const mockStore = configureStore([]);
 

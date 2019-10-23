56b8eee4c4c853019183fb1f967ea76f6dc27fb7
Kaoru Wada <hello@nagomu.me>
2019-10-23 12:36:07 +0900

    Remove unnecessary temporary styles

--- src/config/Routes.tsx
+++ src/config/Routes.tsx
@@ -1,46 +1,16 @@
-/** @jsx jsx */
-import { css, Global, jsx } from '@emotion/core';
 import * as React from 'react';
-import { BrowserRouter, Switch, Redirect, Route } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
-import { useAuth } from '~/hooks/common/useAuth';
 import Dashboard from '~/containers/specifics/dashboard/Dashboard';
 import Login from '~/containers/specifics/login/Login';
-
-const globalStyles = `
-  * {
-    box-sizing: border-box;
-  }
-
-  html {
-    background-color: #fff;
-    color: #212121;
-    font-family: 'Noto Sans CJK JP', -apple-system, BlinkMacSystemFont, Verdana, Meiryo, 'MS PGothic', sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol;
-    font-size: 16px;
-    line-height: 1.15;
-    -webkit-text-size-adjust: 100%;
-  }
-
-  body {
-    margin: 0;
-  }
-
-  html,
-  body {
-    position: relative;
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
-  }
-`;
+import { useAuth } from '~/hooks/common/useAuth';
 
 const Routes: React.FC = () => {
   const { user } = useAuth();
 
   return (
     <BrowserRouter>
-      <Global styles={css(globalStyles)} />
-      {!!user ? (
+      {user ? (
         <Switch>
           <Route path="/dashboard" component={Dashboard} />
           <Redirect from="/" to="/dashboard" />

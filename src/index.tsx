23e31e132d937f44e8a00fb2ec32b4696f407bbc
Kaoru Wada <hello@nagomu.me>
2019-10-23 12:35:34 +0900

    Add global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
-import * as React from 'react';
+import { Global } from '@emotion/core';
+import React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
+import globalStyles from '~/config/globalStyles';
 import Routes from '~/config/Routes';
 import { store } from '~/store';
 
 ReactDOM.render(
   <Provider store={store}>
+    <Global styles={globalStyles} />
     <Routes />
   </Provider>,
   document.getElementById('root') as HTMLElement,

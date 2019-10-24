b069b4757e50b6cdc1413878282b9765b53da14e
Kaoru Wada <hello@nagomu.me>
2019-10-24 16:14:05 +0900

    Add localStorage feature to userAuth

---
+++ src/hooks/specifics/routes/useAuth.ts
@@ -0,0 +1,52 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { AppState } from '~/store';
+import {
+  AuthActions,
+  AuthState,
+  onAuthStateChanged,
+  onSignIn,
+  onSignOut,
+} from '~/store/auth';
+import {
+  getRedirectTo,
+  isAuthenticatedOrWaiting,
+  isAuthenticationWaiting,
+  RedirectState,
+  setIsRedirectingIfNeed,
+} from '~/utils/redirect';
+
+export type UseAuth = AuthState & AuthActions & RedirectState;
+
+export const useAuth = (): UseAuth => {
+  const dispatch = useDispatch();
+  const auth = useSelector((state: AppState) => state.auth);
+
+  useEffect(() => {
+    onAuthStateChanged(dispatch);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('beforeunload', () =>
+      setIsRedirectingIfNeed(auth.user),
+    );
+    return (): void =>
+      window.addEventListener('beforeunload', () =>
+        setIsRedirectingIfNeed(auth.user),
+      );
+  });
+
+  return {
+    user: auth.user,
+    isLoading: auth.isLoading,
+    isAuthenticatedOrWaiting: isAuthenticatedOrWaiting(auth.user),
+    isAuthenticationWaiting: isAuthenticationWaiting(
+      !!auth.isLoading,
+      auth.user,
+    ),
+    redirectTo: getRedirectTo(),
+    onSignIn: (): void => onSignIn(dispatch),
+    onSignOut: (): void => onSignOut(dispatch),
+  };
+};

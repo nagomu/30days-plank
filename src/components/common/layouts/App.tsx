75912ee5355a8d88c97dd715d4f8198c8d5e583f
Kaoru Wada <hello@nagomu.me>
2019-10-25 16:15:17 +0900

    Tweak border styles

--- src/components/common/layouts/App.tsx
+++ src/components/common/layouts/App.tsx
@@ -41,6 +41,10 @@ const Main = styled.main`
   position: relative;
   overflow-x: hidden;
   overflow-y: auto;
+
+  @media all and (min-width: 680px) {
+    border-right: 1px solid #e0e0e0;
+  }
 `;
 
 type OwnProps = {
--- src/components/specifics/dashboard/Workout.tsx
+++ src/components/specifics/dashboard/Workout.tsx
@@ -24,6 +24,10 @@ const StyledContainer = styled(Container)`
   padding: 0;
   border-top: 1px solid #e0e0e0;
   color: ${(props: ContainerProps) => props.color};
+
+  &:first-child {
+    border-top: 0;
+  }
 `;
 /* eslint-enable */
 

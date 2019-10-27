fa4eb244260446ac6b1109518d8e0debac3e73c0
Kaoru Wada <hello@nagomu.me>
2019-10-27 16:06:06 +0900

    Use FloatingActionButton

--- src/components/specifics/workout/StartOrPauseButton.tsx
+++ src/components/specifics/workout/StartOrPauseButton.tsx
@@ -1,52 +1,15 @@
-import styled from '@emotion/styled';
 import * as React from 'react';
+import styled from '@emotion/styled';
 
+import FloatingActionButton from '~/components/common/buttons/FloatingActionButton';
 import Icon from '~/components/common/icons/Icon';
 import { Status } from '~/store/workout';
-import rgba from '~/utils/rgba';
 
-const Button = styled.button`
-  display: flex;
+const Button = styled(FloatingActionButton)`
   position: absolute;
   top: 50%;
   left: 50%;
-  align-items: center;
-  justify-content: center;
-  width: 48px;
-  height: 48px;
-  margin: 0;
-  padding: 0;
   transform: translate(-50%, -50%);
-  transition: box-shadow 0.25s ease-in-out;
-  border: 0;
-  border-radius: 999em;
-  background-color: #1e88e5;
-  box-shadow: 0 2px 4px ${rgba('#000', 0.4)};
-  color: #fff;
-  font-size: 24px;
-
-  &:disabled {
-    background-color: ${rgba('#000', 0.38)};
-    box-shadow: 0 2px 4px ${rgba('#000', 0)};
-    color: ${rgba('#212121', 0.6)};
-  }
-
-  &:not(:disabled) {
-    cursor: pointer;
-  }
-
-  &:focus {
-    outline: none;
-  }
-
-  &:hover:not(:disabled) {
-    box-shadow: 0 2px 8px ${rgba('#000', 0.4)};
-  }
-
-  &:active:not(:disabled),
-  &:focus:not(:disabled) {
-    box-shadow: 0 0 4px ${rgba('#000', 0.4)};
-  }
 `;
 
 type Props = {

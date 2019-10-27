def3932d8bd70721c2cb7337ab069fa74c47123b
Kaoru Wada <hello@nagomu.me>
2019-10-27 15:56:42 +0900

    Add FloatingActionButton

---
+++ src/components/common/buttons/FloatingActionButton.tsx
@@ -0,0 +1,45 @@
+import styled from '@emotion/styled';
+
+import rgba from '~/utils/rgba';
+
+const FloatingActionButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 48px;
+  height: 48px;
+  margin: 0;
+  padding: 0;
+  transition: box-shadow 0.25s ease-in-out;
+  border: 0;
+  border-radius: 999em;
+  background-color: #1e88e5;
+  box-shadow: 0 2px 4px ${rgba('#000', 0.4)};
+  color: #fff;
+  font-size: 24px;
+
+  &:disabled {
+    background-color: ${rgba('#000', 0.38)};
+    box-shadow: 0 2px 4px ${rgba('#000', 0)};
+    color: ${rgba('#212121', 0.6)};
+  }
+
+  &:not(:disabled) {
+    cursor: pointer;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &:hover:not(:disabled) {
+    box-shadow: 0 2px 8px ${rgba('#000', 0.4)};
+  }
+
+  &:active:not(:disabled),
+  &:focus:not(:disabled) {
+    box-shadow: 0 0 4px ${rgba('#000', 0.4)};
+  }
+`;
+
+export default FloatingActionButton;

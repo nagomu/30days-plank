5622ec039c80c7218ef2a52d8aee962422cec20b
Kaoru Wada <hello@nagomu.me>
2019-10-27 16:09:00 +0900

    Cosme

--- src/components/specifics/workout/StartOrPauseButton.tsx
+++ src/components/specifics/workout/StartOrPauseButton.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
 import styled from '@emotion/styled';
+import * as React from 'react';
 
 import FloatingActionButton from '~/components/common/buttons/FloatingActionButton';
 import Icon from '~/components/common/icons/Icon';

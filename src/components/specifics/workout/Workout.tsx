20b2c150d776adf062cf92ff30b418b7abf53e2d
Kaoru Wada <hello@nagomu.me>
2019-10-27 14:58:46 +0900

    Add Workout

---
+++ src/components/specifics/workout/Workout.tsx
@@ -0,0 +1,92 @@
+import styled from '@emotion/styled';
+import * as React from 'react';
+
+import Icon from '~/components/common/icons/Icon';
+import DrawerScreen from '~/components/common/layouts/DrawerScreen';
+import { Props as WorkoutProps } from '~/components/specifics/dashboard/Workout';
+import DonutChart from '~/components/specifics/workout/DonutChart';
+import ResetButton from '~/components/specifics/workout/ResetButton';
+import StartOrPauseButton from '~/components/specifics/workout/StartOrPauseButton';
+import { Status } from '~/containers/specifics/workout/Workout';
+
+const Container = styled.div`
+  position: relative;
+`;
+
+const ButtonGroup = styled.div`
+  position: absolute;
+  bottom: 64px;
+  left: 0;
+  width: 100%;
+
+  @media all and (orientation: landscape) and (max-height: 460px) {
+    bottom: 32px;
+  }
+`;
+
+type OwnProps = {
+  isLoading?: boolean;
+};
+
+export type TimerProps = OwnProps & WorkoutProps;
+
+type StateProps = {
+  progress: number;
+  status: Status;
+};
+
+type HandlerProps = {
+  onStart: () => void;
+  onTogglePause: () => void;
+  onReset: () => void;
+};
+
+type Props = TimerProps & StateProps & HandlerProps;
+
+const Workout: React.FC<Props> = props => {
+  const {
+    isCompleted,
+    isLoading,
+    menu,
+    onReset,
+    onStart,
+    onTogglePause,
+    pathname,
+    progress,
+    status,
+    title,
+  } = props;
+
+  return (
+    <DrawerScreen
+      title={`${title} - ${menu} sec`}
+      pathname={pathname || '/dashboard'}
+      icon={
+        isCompleted ? (
+          <Icon name="done" style={{ width: '24px', height: '24px' }} />
+        ) : (
+          undefined
+        )
+      }
+      isLoading={isLoading}
+    >
+      <Container>
+        <DonutChart progress={progress} seconds={menu} status={status} />
+        <ButtonGroup>
+          <StartOrPauseButton
+            onStart={onStart}
+            onTogglePause={onTogglePause}
+            status={status}
+          />
+          {status !== Status.standby && (
+            <ResetButton role="button" onClick={onReset}>
+              Reset
+            </ResetButton>
+          )}
+        </ButtonGroup>
+      </Container>
+    </DrawerScreen>
+  );
+};
+
+export default Workout;
---
+++ src/components/specifics/workout/__tests__/Workout.test.tsx
@@ -0,0 +1,49 @@
+import firebase from 'firebase';
+
+import Workout from '~/components/specifics/workout/Workout';
+import { Status } from '~/containers/specifics/workout/Workout';
+import { withProvider } from '~/utils/testHelpers';
+
+describe('Timer', () => {
+  const props = {
+    id: '1',
+    isCompleted: false,
+    isLoading: false,
+    isRest: false,
+    menu: 20,
+    onReset: jest.fn(),
+    onStart: jest.fn(),
+    onTogglePause: jest.fn(),
+    pathname: '/',
+    progress: 20,
+    scheduledDate: firebase.firestore.Timestamp.fromDate(new Date()),
+    status: Status.standby,
+    title: 'Day 1',
+  };
+
+  it('renders correctly', () => {
+    const params = {
+      Component: Workout,
+      props,
+    };
+    const wrapper = withProvider(params).find(Workout);
+    expect(wrapper.find('DrawerScreen').length).toEqual(1);
+    expect(wrapper.find('DonutChart').length).toEqual(1);
+    expect(wrapper.find('StartOrPauseButton').length).toEqual(1);
+  });
+
+  describe('if started', () => {
+    it('renders correctly', () => {
+      const params = {
+        Component: Workout,
+        props: {
+          ...props,
+          status: Status.start,
+        },
+      };
+      const wrapper = withProvider(params).find(Workout);
+      const resetButton = wrapper.find('button').at(1);
+      expect(resetButton.text()).toEqual('Reset');
+    });
+  });
+});

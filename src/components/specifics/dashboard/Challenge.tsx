9c813d6f748b007680a047ed779b2adde39e04c2
Kaoru Wada <hello@nagomu.me>
2019-10-28 18:40:06 +0900

    Graduating Challenge from a mock

--- src/components/specifics/dashboard/Challenge.tsx
+++ src/components/specifics/dashboard/Challenge.tsx
@@ -1,10 +1,8 @@
 import styled from '@emotion/styled';
 import * as React from 'react';
 
-import Workout, {
-  Props as WorkoutProps,
-} from '~/components/specifics/dashboard/Workout';
-import { timestampFromDate } from '~/utils/firebase';
+import Workout from '~/components/specifics/dashboard/Workout';
+import { Workout as WorkoutType } from '~/store/workout';
 
 const Workouts = styled.ul`
   margin: 0;
@@ -12,28 +10,20 @@ const Workouts = styled.ul`
   list-style: none;
 `;
 
-const Challenge: React.FC = () => {
-  const generateMock = (): WorkoutProps[] => {
-    const arr = Array.from(Array(30), (_v, k) => k + 1);
-
-    return arr.map(i => ({
-      id: `${i}`,
-      isCompleted: true,
-      isRest: i === 6 || i === 13 || i === 19 || i === 26,
-      menu: 20,
-      pathname: `/challenges/1/workouts/${i}`,
-      scheduledDate: timestampFromDate(new Date(2019, 9, i)),
-      title: i === 6 || i === 13 || i === 19 || i === 26 ? 'REST' : `Day ${i}`,
-    }));
-  };
-
-  return (
-    <Workouts>
-      {generateMock().map(props => (
-        <Workout key={props.id} {...props} />
-      ))}
-    </Workouts>
-  );
+type Props = {
+  workouts: WorkoutType[];
 };
+
+const Challenge: React.FC<Props> = ({ workouts }) => (
+  <Workouts>
+    {workouts.map(workout => (
+      <Workout
+        {...workout}
+        key={workout.id}
+        pathname={`/workouts/${workout.id}`}
+      />
+    ))}
+  </Workouts>
+);
 
 export default Challenge;

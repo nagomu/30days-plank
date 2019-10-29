13b784362c4d27c95ba3e6fc5469fd08f82de79e
Kaoru Wada <hello@nagomu.me>
2019-10-29 17:56:55 +0900

    Refactor Workout container

--- src/containers/specifics/workout/Workout.tsx
+++ src/containers/specifics/workout/Workout.tsx
@@ -1,16 +1,11 @@
 import * as React from 'react';
-import { RouteComponentProps } from 'react-router-dom';
 
-import Loading from '~/components/common/loaders/Loading';
 import WrappedComponent from '~/containers/specifics/workout/WorkoutTimer';
 import { useWorkout } from '~/hooks/common/useWorkout';
 
-type Props = RouteComponentProps<{ id: string }>;
-
-const Workout: React.FC<Props> = props => {
-  const { isLoading, workout } = useWorkout(props.match.params.id);
-  if (!workout || isLoading) return <Loading />;
-  return <WrappedComponent {...workout} isLoading={isLoading} />;
+const Workout: React.FC = () => {
+  const { isLoading, workout } = useWorkout();
+  return <WrappedComponent workout={workout} isLoading={isLoading} />;
 };
 
 export default Workout;
---
+++ src/containers/specifics/workout/__tests__/Workout.test.tsx
@@ -0,0 +1,32 @@
+import Workout from '~/containers/specifics/workout/Workout';
+import { mockStore, withProvider } from '~/utils/testHelpers';
+
+describe('WorkoutContainer', () => {
+  const props = {
+    user: { uid: 'xxx' },
+  };
+
+  const store = {
+    auth: {
+      user: undefined,
+      isLoading: undefined,
+    },
+    challenge: {
+      challenge: undefined,
+      isLoading: undefined,
+    },
+    workout: {
+      isLoading: undefined,
+    },
+  };
+
+  it('renders correctly', () => {
+    const wrapper = withProvider({
+      Component: Workout,
+      props,
+      store: mockStore(store),
+    }).find('Workout');
+    expect(wrapper.find('WorkoutTimer').prop('isLoading')).toEqual(true);
+    expect(wrapper.find('WorkoutTimer').prop('workout')).toEqual(undefined);
+  });
+});

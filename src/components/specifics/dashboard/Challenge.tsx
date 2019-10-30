37b91a75367e7e62e03a6afc5825107de26061bf
Kaoru Wada <hello@nagomu.me>
2019-10-30 21:05:10 +0900

    Show archive button to dashboard if expired

--- src/components/specifics/dashboard/Challenge.tsx
+++ src/components/specifics/dashboard/Challenge.tsx
@@ -1,8 +1,15 @@
 import styled from '@emotion/styled';
 import * as React from 'react';
 
+import PrimaryButton from '~/components/common/buttons/PrimaryButton';
 import Workout from '~/components/specifics/dashboard/Workout';
+import { useOnArchive } from '~/hooks/specifics/dashboard/useOnArchive';
 import { Workout as WorkoutType } from '~/store/workout';
+
+const Expired = styled.div`
+  padding: 0 16px;
+  border-bottom: 1px solid #e0e0e0;
+`;
 
 const Workouts = styled.ul`
   margin: 0;
@@ -15,16 +22,29 @@ type Props = {
   workouts: WorkoutType[];
 };
 
-const Challenge: React.FC<Props> = ({ challengeId, workouts }) => (
-  <Workouts>
-    {workouts.map(workout => (
-      <Workout
-        {...workout}
-        key={workout.id}
-        pathname={`/challenges/${challengeId}/workouts/${workout.id}`}
-      />
-    ))}
-  </Workouts>
-);
+const Challenge: React.FC<Props> = ({ challengeId, workouts }) => {
+  const { isExpired, onArchive } = useOnArchive();
 
+  return (
+    <>
+      {isExpired && (
+        <Expired>
+          <p>This challenge seems to be out of date 😢</p>
+          <p>
+            <PrimaryButton onClick={onArchive}>Archive now</PrimaryButton>
+          </p>
+        </Expired>
+      )}
+      <Workouts>
+        {workouts.map(workout => (
+          <Workout
+            {...workout}
+            key={workout.id}
+            pathname={`/challenges/${challengeId}/workouts/${workout.id}`}
+          />
+        ))}
+      </Workouts>
+    </>
+  );
+};
 export default Challenge;

f8cc4477a9f87bdbfc1d6113ca99d88f75bd02e5
Kaoru Wada <hello@nagomu.me>
2019-10-30 22:39:04 +0900

    Add Archives component

---
+++ src/components/specifics/archive/Archives.tsx
@@ -0,0 +1,96 @@
+import styled from '@emotion/styled';
+import * as React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import Icon from '~/components/common/icons/Icon';
+import DrawerScreen from '~/components/common/layouts/DrawerScreen';
+import { Archive } from '~/store/archive';
+import rgba from '~/utils/rgba';
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  border-bottom: 1px solid #e0e0e0;
+  list-style: none;
+`;
+
+const Menu = styled.li`
+  display: block;
+  margin: 0;
+  padding: 0;
+  border-top: 1px solid #e0e0e0;
+  color: #212121;
+
+  &:first-of-type {
+    border-top: 0;
+  }
+`;
+
+const Link = styled(NavLink)`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  color: ${rgba('#212121', 0.6)};
+  font-size: 12px;
+  text-decoration: none;
+`;
+
+const Completed = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  width: 48px;
+  min-height: 48px;
+  margin: 0;
+  padding: 12px 8px 12px 16px;
+  color: #e0e0e0;
+  font-size: 24px;
+
+  &.isCompleted {
+    color: #4caf50;
+  }
+`;
+
+const Title = styled.span`
+  display: inline-block;
+  margin-right: 4px;
+  font-size: 16px;
+  font-weight: 500;
+`;
+
+type Props = {
+  archives?: Archive[];
+  isLoading: boolean;
+};
+
+const Archives: React.FC<Props> = ({ archives, isLoading }) => (
+  <DrawerScreen title="Archives" pathname="/dashboard" isLoading={isLoading}>
+    {archives && archives.length > 0 ? (
+      <List>
+        {archives.map((archive: Archive) => (
+          <Menu key={archive.id}>
+            <Link to={`/challenges/${archive.challengeId}`} role="button">
+              <Completed
+                className={archive.achievementRate >= 80 ? 'isCompleted' : ''}
+              >
+                <Icon name="done" />
+              </Completed>
+              <Title>{archive.title}</Title>
+              {` - ${archive.achievementRate}%`}
+            </Link>
+          </Menu>
+        ))}
+      </List>
+    ) : (
+      <>
+        {/* TODO: Replace tp empty component */}
+        <p style={{ padding: '0 16px' }}>There is no archive yet.</p>
+      </>
+    )}
+  </DrawerScreen>
+);
+
+export default Archives;
---
+++ src/components/specifics/archive/__tests__/Archives.test.tsx
@@ -0,0 +1,51 @@
+import Archives from '~/components/specifics/archive/Archives';
+import { timestampFromDate } from '~/utils/firebase';
+import { withProvider } from '~/utils/testHelpers';
+
+describe('Archives', () => {
+  const props = {
+    archives: [
+      {
+        achievementRate: 80,
+        challengeId: '1',
+        createdAt: timestampFromDate(new Date()),
+        id: '1',
+        title: 'title1',
+      },
+      {
+        achievementRate: 100,
+        challengeId: '2',
+        createdAt: timestampFromDate(new Date()),
+        id: '2',
+        title: 'title1',
+      },
+      {
+        achievementRate: 79,
+        challengeId: '3',
+        createdAt: timestampFromDate(new Date()),
+        id: '3',
+        title: 'title1',
+      },
+    ],
+    isLoading: false,
+  };
+
+  it('renders correctly', () => {
+    const wrapper = withProvider({ Component: Archives, props });
+
+    expect(wrapper.find('DrawerScreen').length).toEqual(1);
+    expect(wrapper.find('ul NavLink').length).toEqual(3);
+    expect(wrapper.find('span.isCompleted').length).toEqual(2);
+  });
+
+  it('renders message correctly if archives are empty', () => {
+    const _props = {
+      ...props,
+      archives: [],
+    };
+    const wrapper = withProvider({ Component: Archives, props: _props });
+
+    expect(wrapper.find('DrawerScreen').length).toEqual(1);
+    expect(wrapper.text()).toContain('There is no archive yet.');
+  });
+});

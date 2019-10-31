29f745b2e2fbb52776fa52989a0b3a3697d963fc
Kaoru Wada <hello@nagomu.me>
2019-10-31 17:16:46 +0900

    Avoid test failures due to date differences

--- src/store/archive/utils/__tests__/mergeArchives.test.ts
+++ src/store/archive/utils/__tests__/mergeArchives.test.ts
@@ -1,5 +1,10 @@
+import timekeeper from 'timekeeper';
+
 import { mergeArchives } from '~/store/archive/utils/mergeArchives';
 import { timestampFromDate } from '~/utils/firebase';
+
+const mockToday = new Date(Date.UTC(2018, 0, 1, 0, 0, 0));
+timekeeper.freeze(mockToday);
 
 describe('mergeArchives', () => {
   it('correctly', () => {
@@ -7,21 +12,21 @@ describe('mergeArchives', () => {
       {
         achievementRate: 80,
         challengeId: '1',
-        createdAt: timestampFromDate(new Date()),
+        createdAt: timestampFromDate(mockToday),
         id: '1',
         title: 'title1',
       },
       {
         achievementRate: 100,
         challengeId: '2',
-        createdAt: timestampFromDate(new Date()),
+        createdAt: timestampFromDate(mockToday),
         id: '2',
         title: 'title2',
       },
       {
         achievementRate: 79,
         challengeId: '3',
-        createdAt: timestampFromDate(new Date()),
+        createdAt: timestampFromDate(mockToday),
         id: '3',
         title: 'title3',
       },
@@ -31,28 +36,28 @@ describe('mergeArchives', () => {
       {
         achievementRate: 79,
         challengeId: '4',
-        createdAt: timestampFromDate(new Date()),
+        createdAt: timestampFromDate(mockToday),
         id: '4',
         title: 'title4',
       },
       {
         achievementRate: 79,
         challengeId: '5',
-        createdAt: timestampFromDate(new Date()),
+        createdAt: timestampFromDate(mockToday),
         id: '5',
         title: 'title5',
       },
       {
         achievementRate: 80,
         challengeId: '1',
-        createdAt: timestampFromDate(new Date()),
+        createdAt: timestampFromDate(mockToday),
         id: '1',
         title: 'title1',
       },
       {
         achievementRate: 100,
         challengeId: '2',
-        createdAt: timestampFromDate(new Date()),
+        createdAt: timestampFromDate(mockToday),
         id: '2',
         title: 'title2',
       },
@@ -62,35 +67,35 @@ describe('mergeArchives', () => {
       {
         achievementRate: 80,
         challengeId: '1',
-        createdAt: timestampFromDate(new Date()),
+        createdAt: timestampFromDate(mockToday),
         id: '1',
         title: 'title1',
       },
       {
         achievementRate: 100,
         challengeId: '2',
-        createdAt: timestampFromDate(new Date()),
+        createdAt: timestampFromDate(mockToday),
         id: '2',
         title: 'title2',
       },
       {
         achievementRate: 79,
         challengeId: '3',
-        createdAt: timestampFromDate(new Date()),
+        createdAt: timestampFromDate(mockToday),
         id: '3',
         title: 'title3',
       },
       {
         achievementRate: 79,
         challengeId: '4',
-        createdAt: timestampFromDate(new Date()),
+        createdAt: timestampFromDate(mockToday),
         id: '4',
         title: 'title4',
       },
       {
         achievementRate: 79,
         challengeId: '5',
-        createdAt: timestampFromDate(new Date()),
+        createdAt: timestampFromDate(mockToday),
         id: '5',
         title: 'title5',
       },
@@ -99,3 +104,5 @@ describe('mergeArchives', () => {
     expect(mergeArchives(state, store)).toEqual(expected);
   });
 });
+
+timekeeper.reset();

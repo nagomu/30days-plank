4b326e9ab634c8f866e29f697eb54133280e8fda
Kaoru Wada <hello@nagomu.me>
2019-11-01 21:06:06 +0900

    Add challenges collection

--- src/utils/firestore/__tests__/collections.test.ts
+++ src/utils/firestore/__tests__/collections.test.ts
@@ -1,4 +1,4 @@
-import { archives } from '~/utils/firestore/collections';
+import { archives, challenges } from '~/utils/firestore/collections';
 
 // NOTE: Avoid using mock (~/utils/firestore/__mocks__/collections.mock.ts)
 jest.unmock('~/utils/firestore/collections');
@@ -6,5 +6,9 @@ jest.unmock('~/utils/firestore/collections');
 describe('utils: firestore', () => {
   it('sets correct archives collectionPath', () => {
     expect(archives('uid').path).toEqual('users/uid/archives');
+  });
+
+  it('sets correct challenges collectionPath', () => {
+    expect(challenges('uid').path).toEqual('users/uid/challenges');
   });
 });
--- src/utils/firestore/collections.ts
+++ src/utils/firestore/collections.ts
@@ -2,3 +2,6 @@ import firebase, { CollectionReference } from '~/utils/firebase';
 
 export const archives = (uid: string): CollectionReference =>
   firebase.firestore().collection(`/users/${uid}/archives`);
+
+export const challenges = (uid: string): CollectionReference =>
+  firebase.firestore().collection(`/users/${uid}/challenges`);

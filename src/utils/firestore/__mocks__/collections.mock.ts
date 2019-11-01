80703114962280da632116114983682594a2484b
Kaoru Wada <hello@nagomu.me>
2019-11-01 21:05:48 +0900

    Add mock challenges collection

--- src/utils/firestore/__mocks__/collections.mock.ts
+++ src/utils/firestore/__mocks__/collections.mock.ts
@@ -13,5 +13,46 @@ jest.mock('../collections', () => ({
       }),
     ),
   }),
+  challenges: jest.fn().mockReturnValue({
+    doc: jest.fn().mockReturnValue({
+      update: jest.fn().mockReturnValue(Promise.resolve),
+    }),
+    add: jest.fn().mockReturnValue(
+      Promise.resolve({
+        get: jest.fn().mockReturnValue(
+          Promise.resolve({
+            exists: true,
+            id: 'id',
+          }),
+        ),
+      }),
+    ),
+    get: jest.fn().mockReturnValue(
+      Promise.resolve({
+        empty: false,
+        forEach: (callback: any) =>
+          callback({
+            id: 'id',
+            data: () => ({ data: 'data' }),
+          }),
+      }),
+    ),
+    orderBy: jest.fn().mockReturnValue({
+      where: jest.fn().mockReturnValue({
+        limit: jest.fn().mockReturnValue({
+          get: jest.fn().mockReturnValue(
+            Promise.resolve({
+              empty: false,
+              forEach: (callback: any) =>
+                callback({
+                  id: 'id',
+                  data: () => ({ data: 'data' }),
+                }),
+            }),
+          ),
+        }),
+      }),
+    }),
+  }),
 }));
 /* eslint-enable */

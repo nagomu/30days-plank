f8fd6d4287bfdf829f074834a745a888f18ea206
Kaoru Wada <hello@nagomu.me>
2019-11-02 00:19:57 +0900

    Remove old Firestore utils

--- src/services/firebase/setUserToFirestore.ts
+++
@@ -1,9 +0,0 @@
-import { User } from '~/store/auth';
-import firebase from '~/utils/firebase';
-
-const setUserToFirestore = async (user: User): Promise<void> => {
-  const collection = firebase.firestore().collection('/users');
-  return await collection.doc(user.uid).set(user);
-};
-
-export default setUserToFirestore;

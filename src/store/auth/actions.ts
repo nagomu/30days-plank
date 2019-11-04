11e17a09ca3fbf320b6dbaf9e483e1eeb4888c0d
Kaoru Wada <hello@nagomu.me>
2019-11-04 22:05:18 +0900

    Use asyncOnAuthStateChanged

--- src/store/auth/actions.ts
+++ src/store/auth/actions.ts
@@ -2,6 +2,7 @@ import { Dispatch } from 'redux';
 
 import { clearRedirectStorage, setIsAuthenticating } from '~/services/auth';
 import { firebase, FirebaseUser } from '~/services/firebase';
+import { asyncOnAuthStateChanged } from '~/services/firebase/asyncOnAuthStateChanged';
 import { postError, users } from '~/services/firestore';
 import {
   ADD_USER,
@@ -103,17 +104,16 @@ export const onFetchUser = async (
   return;
 };
 
-export const onAuthStateChanged = (dispatch: Dispatch): void => {
+export const onAuthStateChanged = async (dispatch: Dispatch): Promise<void> => {
   try {
     dispatch(observeAuthStateChanged());
-    firebase.auth().onAuthStateChanged((user: FirebaseUser | null) => {
-      if (user) {
-        onFetchUser(dispatch, user);
-      } else {
-        clearRedirectStorage();
-      }
-      dispatch(authStateChanged());
-    });
+    const user = await asyncOnAuthStateChanged();
+    if (user) {
+      onFetchUser(dispatch, user);
+    } else {
+      clearRedirectStorage();
+    }
+    dispatch(authStateChanged());
   } catch (error) {
     postError(error);
   }

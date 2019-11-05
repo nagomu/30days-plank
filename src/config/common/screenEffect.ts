5983d4c54f9a336ebb789f9c54262b7963db0ebc
Kaoru Wada <hello@nagomu.me>
2019-11-06 01:07:58 +0900

    Avoid flickering of background-color

--- src/config/common/screenEffect.ts
+++ src/config/common/screenEffect.ts
@@ -3,12 +3,10 @@ import { css, keyframes } from '@emotion/core';
 const popup = keyframes`
   0% {
     transform: translateY(16px);
-    opacity: 0.5;
   }
 
   100% {
     transform: translateY(0);
-    opacity: 1;
   }
 `;
 

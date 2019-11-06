2d870da31c359ab710956e71673ceca0143221ae
Kaoru Wada <hello@nagomu.me>
2019-11-06 18:50:54 +0900

    Add sample code

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,58 @@
-import * as functions from 'firebase-functions';
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const serviceAccount = require(`${__dirname}/../serviceAccounts.json`);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://thirty-days-plank-v1.firebaseio.com',
+});
+
+const typeDefs = gql`
+  type Archive {
+    id: String!
+    achievementRate: Int!
+    challengeId: String!
+    title: String!
+  }
+
+  type Query {
+    archives(uid: String!): [Archive]
+  }
+`;
+
+type Archive = {
+  id: string;
+  challengeId: string;
+  title: string;
+  achievementRate: number;
+};
+
+const resolvers = {
+  Query: {
+    // eslint-disable-next-line
+    archives: async (_obj: any, args: any, _context: any, _info: any) => {
+      const db = admin.firestore();
+      const ref = db.collection(`/users/${args.uid}/archives`);
+      const snapshot = await ref.get();
+
+      const results: Archive[] = [];
+
+      // eslint-disable-next-line
+      snapshot.forEach((doc: any) => {
+        results.push({
+          id: doc.id,
+          ...doc.data(),
+        } as Archive);
+      });
+
+      return results;
+    },
+  },
+};
+
+const app = express();
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app, path: '/', cors: true });
+exports.graphql = functions.https.onRequest(app);

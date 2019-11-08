7dc4867eb5ef07cf107e0f5d37bfd9040d85abf8
Kaoru Wada <hello@nagomu.me>
2019-11-08 20:35:33 +0900

    Add challenge service

---
+++ src/services/firebase/__tests__/challenge.test.ts
@@ -0,0 +1,137 @@
+import timekeeper from 'timekeeper';
+
+import { Workout } from '~/services/firebase/workout';
+
+import {
+  addChallenge,
+  Challenge,
+  fetchChallenge,
+  updateChallenge,
+} from '../challenge';
+import { workouts } from './fixtures/workouts';
+
+jest.mock('~/utils/datetime');
+
+const now = new Date(Date.UTC(2019, 9, 1, 0, 0, 0));
+timekeeper.freeze(now);
+
+const mockGet = jest.fn();
+const mockAdd = jest.fn();
+const mockUpdate = jest.fn();
+
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+jest.mock(
+  '~/services/firebase',
+  jest.fn().mockReturnValue({
+    firebase: {
+      firestore: () => ({
+        batch: () => ({
+          commit: mockAdd,
+          set: jest.fn(),
+        }),
+        collection: () => ({
+          doc: (id?: string) => ({
+            get: mockGet(id || 'new-id'),
+            update: mockUpdate,
+          }),
+        }),
+      }),
+    },
+  }),
+);
+/* eslint-enable */
+
+describe('challenge', () => {
+  const ts = {
+    seconds: 1569855600,
+    nanoseconds: 0,
+  } as firebase.firestore.Timestamp;
+
+  let fixture: Challenge[];
+  beforeEach(() => {
+    fixture = [
+      {
+        id: 'cid',
+        description: 'description',
+        isActive: true,
+        workouts: [
+          {
+            id: 'wid',
+            title: 'title',
+            menu: 20,
+            date: ts,
+            isCompleted: false,
+            isRest: false,
+            createdAt: ts,
+            updatedAt: ts,
+          },
+        ],
+        createdAt: ts,
+        updatedAt: ts,
+      },
+    ];
+  });
+
+  mockGet.mockImplementation((id: string) => {
+    const result = fixture.find(c => c.id === id);
+    return jest.fn().mockResolvedValue({ data: () => result });
+  });
+
+  mockUpdate.mockImplementation(challenge => {
+    const index = fixture.findIndex(data => data.id === 'cid');
+    fixture[index] = {
+      ...fixture[index],
+      isActive: challenge.isActive,
+    };
+    return jest.fn().mockResolvedValue(undefined);
+  });
+
+  describe('fetchChallenge', () => {
+    it('returns correct ChallengeData if it exists', async () => {
+      const expected = fixture[0];
+      expect(await fetchChallenge('uid', 'cid')).toEqual(expected);
+    });
+
+    it('returns undefined if it does not exist', async () => {
+      const expected = undefined;
+      expect(await fetchChallenge('uid', 'id')).toEqual(expected);
+    });
+  });
+
+  describe('addChallenge', () => {
+    const challenge = {
+      id: 'new-id',
+      description: undefined,
+      isActive: true,
+      workouts: workouts as Workout[],
+      createdAt: ts,
+      updatedAt: ts,
+    };
+
+    it('add ChallengeData correctly', async () => {
+      mockAdd.mockImplementation(() => {
+        fixture.push(challenge);
+        return jest.fn().mockResolvedValue(undefined);
+      });
+      expect(await addChallenge('uid')).toEqual(challenge);
+    });
+  });
+
+  describe('updateChallenge', () => {
+    const params = {
+      id: 'cid',
+      isActive: false,
+    };
+
+    it('updates ChallengeData correctly', async () => {
+      const expected = {
+        ...fixture[0],
+        isActive: false,
+      };
+
+      expect(await updateChallenge('uid', 'cid', params)).toEqual(expected);
+    });
+  });
+});
+
+timekeeper.reset();
---
+++ src/services/firebase/challenge.ts
@@ -0,0 +1,77 @@
+import { workoutFactory } from '~/factories/workoutFactory';
+import { firebase } from '~/services/firebase';
+import { timestamp } from '~/utils/datetime';
+
+import { Workout } from './workout';
+
+type Timestamp = firebase.firestore.Timestamp;
+
+export type Challenge = {
+  id: string;
+  description?: string;
+  isActive: boolean;
+  workouts: Workout[];
+  createdAt?: Timestamp;
+  updatedAt?: Timestamp;
+};
+
+type Params = {
+  id: string;
+  description?: string;
+  isActive: boolean;
+};
+
+type ReturnValue = Promise<Challenge | void>;
+
+export const fetchChallenge = async (uid: string, id: string): ReturnValue => {
+  const collectionPath = `/users/${uid}/challenges`;
+  const ref = firebase.firestore().collection(collectionPath);
+  const snapshot = await ref.doc(id).get();
+  const challenge = snapshot.data() as Challenge | undefined;
+
+  if (challenge) return challenge;
+};
+
+export const addChallenge = async (uid: string): ReturnValue => {
+  const collectionPath = `/users/${uid}/challenges`;
+  const db = firebase.firestore();
+  const batch = db.batch();
+  const ref = db.collection(collectionPath);
+  const id = ref.doc().id;
+  const now = new Date(Date.now());
+  const ts = timestamp(now);
+  const params = {
+    isActive: true,
+    createdAt: ts,
+    updatedAt: ts,
+  };
+
+  batch.set(ref.doc(id), params);
+
+  workoutFactory(now).forEach(params => {
+    const collectionPath = `/users/${uid}/challenges/${id}/workouts`;
+    const wref = firebase.firestore().collection(collectionPath);
+    const wid = wref.doc().id;
+    batch.set(wref.doc(wid), params);
+  });
+
+  await batch.commit();
+
+  return await fetchChallenge(uid, id);
+};
+
+export const updateChallenge = async (
+  uid: string,
+  id: string,
+  challenge: Params,
+): ReturnValue => {
+  const collectionPath = `/users/${uid}/challenges`;
+  const ref = firebase.firestore().collection(collectionPath);
+  const params = {
+    isActive: challenge.isActive,
+    updatedAt: timestamp(new Date(Date.now())),
+  };
+  await ref.doc(id).update(params);
+
+  return await fetchChallenge(uid, id);
+};

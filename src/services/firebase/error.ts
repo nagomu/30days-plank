365b062db4e853f31d69d87b43bf75790f55805c
Kaoru Wada <hello@nagomu.me>
2019-11-08 18:44:43 +0900

    Add error service

---
+++ src/services/firebase/error.ts
@@ -0,0 +1,13 @@
+import { firebase } from '~/services/firebase';
+import { timestamp } from '~/utils/datetime';
+
+export const postError = async (error: Error): Promise<void> => {
+  const params = {
+    message: error.message,
+    stack: error.stack,
+    navigator: { ...window.navigator },
+    timestamp: timestamp(new Date(Date.now())),
+  };
+  const ref = firebase.firestore().collection(`/errors`);
+  await ref.add(params);
+};

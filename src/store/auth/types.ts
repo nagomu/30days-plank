6f50e4258c78bdeaea2aa8d1850e6fddba367efe
Kaoru Wada <hello@nagomu.me>
2019-11-13 13:38:20 +0900

    Remove unnecessary export

--- src/store/auth/types.ts
+++ src/store/auth/types.ts
@@ -11,22 +11,22 @@ export type AuthActions = {
 };
 
 export const OBSERVE_AUTH_STATE_CHANGED = 'OBSERVE_AUTH_STATE_CHANGED';
-export type ObserveAuthStateChangedAction = {
+type ObserveAuthStateChangedAction = {
   type: typeof OBSERVE_AUTH_STATE_CHANGED;
 };
 
 export const AUTH_STATE_CHANGED = 'AUTH_STATE_CHANGED';
-export type AuthStateChangedAction = {
+type AuthStateChangedAction = {
   type: typeof AUTH_STATE_CHANGED;
 };
 
 export const FETCH_USER = 'FETCH_USER';
-export type FetchUserAction = {
+type FetchUserAction = {
   type: typeof FETCH_USER;
 };
 
 export const SET_USER = 'SET_USER';
-export type SetUserAction = {
+type SetUserAction = {
   type: typeof SET_USER;
   payload: {
     user?: User;
@@ -34,22 +34,22 @@ export type SetUserAction = {
 };
 
 export const ADD_USER = 'ADD_USER';
-export type AddUserAction = {
+type AddUserAction = {
   type: typeof ADD_USER;
 };
 
 export const ADD_USER_SUCCESS = 'ADD_USER_SUCCESS';
-export type AddUserSuccessAction = {
+type AddUserSuccessAction = {
   type: typeof ADD_USER_SUCCESS;
 };
 
 export const SIGN_IN = 'SIGN_IN';
-export type SignInAction = {
+type SignInAction = {
   type: typeof SIGN_IN;
 };
 
 export const SIGN_OUT = 'SIGN_OUT';
-export type SignOutAction = {
+type SignOutAction = {
   type: typeof SIGN_OUT;
 };
 

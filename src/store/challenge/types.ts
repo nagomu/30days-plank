254c6fe6a88ae23ca80932a8752b12980c5e9d04
Kaoru Wada <hello@nagomu.me>
2019-11-13 13:23:08 +0900

    Remove unnecessary export

--- src/store/challenge/types.ts
+++ src/store/challenge/types.ts
@@ -12,12 +12,12 @@ export type UpdateChallengeParams = {
 };
 
 export const FETCH_CHALLENGE = 'FETCH_CHALLENGE';
-export type FetchChallengeAction = {
+type FetchChallengeAction = {
   type: typeof FETCH_CHALLENGE;
 };
 
 export const SET_CHALLENGE = 'SET_CHALLENGE';
-export type SetChallengeAction = {
+type SetChallengeAction = {
   type: typeof SET_CHALLENGE;
   payload: {
     challenge?: Challenge;
@@ -25,7 +25,7 @@ export type SetChallengeAction = {
 };
 
 export const SET_PARTIAL_WORKOUT = 'SET_PARTIAL_WORKOUT';
-export type SetPartialWorkoutAction = {
+type SetPartialWorkoutAction = {
   type: typeof SET_PARTIAL_WORKOUT;
   payload: {
     workout: Workout;
@@ -33,22 +33,22 @@ export type SetPartialWorkoutAction = {
 };
 
 export const ADD_CHALLENGE = 'ADD_CHALLENGE';
-export type AddChallengeAction = {
+type AddChallengeAction = {
   type: typeof ADD_CHALLENGE;
 };
 
 export const ADD_CHALLENGE_SUCCESS = 'ADD_CHALLENGE_SUCCESS';
-export type AddChallengeSuccessAction = {
+type AddChallengeSuccessAction = {
   type: typeof ADD_CHALLENGE_SUCCESS;
 };
 
 export const UPDATE_CHALLENGE = 'UPDATE_CHALLENGE';
-export type UpdateChallengeAction = {
+type UpdateChallengeAction = {
   type: typeof UPDATE_CHALLENGE;
 };
 
 export const UPDATE_CHALLENGE_SUCCESS = 'UPDATE_CHALLENGE_SUCCESS';
-export type UpdateChallengeSuccessAction = {
+type UpdateChallengeSuccessAction = {
   type: typeof UPDATE_CHALLENGE_SUCCESS;
 };
 

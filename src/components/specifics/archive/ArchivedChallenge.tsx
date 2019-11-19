2baeef7b2472d64600da700096d940ea7a25b88d
Kaoru Wada <hello@nagomu.me>
2019-11-19 15:12:37 +0900

    Add ArchivedChallenge component

---
+++ src/components/specifics/archive/ArchivedChallenge.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+
+import Workouts from '~/components/common/challenges/Workouts';
+import NotFound from '~/components/common/errors/NotFound';
+import DrawerScreen from '~/components/common/layouts/DrawerScreen';
+import { Challenge } from '~/types';
+
+type Props = {
+  challenge?: Challenge;
+  challengeId?: string;
+  isLoading: boolean;
+  title: string;
+};
+
+const ArchivedChallenge: React.FC<Props> = props => {
+  const { challenge, challengeId, isLoading, title } = props;
+
+  return (
+    <DrawerScreen title={title} pathname="/archives" isLoading={isLoading}>
+      {!challengeId || !challenge ? (
+        <NotFound />
+      ) : (
+        <Workouts
+          challengeId={challengeId}
+          todaysWorkout={undefined}
+          workouts={challenge.workouts}
+        />
+      )}
+    </DrawerScreen>
+  );
+};
+
+export default ArchivedChallenge;

9608a878b0824c4f7053bb1dbcd5868e96c21597
Kaoru Wada <hello@nagomu.me>
2019-11-19 13:22:36 +0900

    Add temporary container of archived challenge

--- src/components/common/Routes.tsx
+++ src/components/common/Routes.tsx
@@ -12,6 +12,11 @@ const NotFound = loadable(() => import('~/components/common/errors/NotFound'), {
   fallback: <Loading />,
 });
 
+const ArchivedChallenge = loadable(
+  () => import('~/containers/specifics/archive/ArchivedChallenge'),
+  { fallback: <Loading /> },
+);
+
 const Archives = loadable(
   () => import('~/containers/specifics/archive/Archives'),
   { fallback: <Loading /> },
@@ -60,6 +65,11 @@ const Routes: React.FC = () => {
             <Switch>
               <Route exact path="/dashboard" component={Dashboard} />
               <Route exact path="/archives" component={Archives} />
+              <Route
+                exact
+                path="/archives/:challengeId"
+                component={ArchivedChallenge}
+              />
               <Route
                 exact
                 path="/challenges/:challengeId/workouts/:id"

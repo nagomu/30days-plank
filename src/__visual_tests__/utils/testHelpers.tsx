eed0458959d62f7a6042bb185b8690d4fab1ddb1
Kaoru Wada <hello@nagomu.me>
2019-11-20 23:01:50 +0900

    Avoid animations if vrt

--- src/__visual_tests__/utils/testHelpers.tsx
+++ src/__visual_tests__/utils/testHelpers.tsx
@@ -26,6 +26,16 @@ export const injectAppToHtml = (component: React.ReactNode): string => {
   let css = fs.readFileSync(cssPath).toString();
   css = `
   ${css}
+  *,
+  *::after,
+  *::before {
+    transition-delay: 0s !important;
+    transition-duration: 0s !important;
+    animation-delay: -0.0001s !important;
+    animation-duration: 0s !important;
+    animation-play-state: paused !important;
+    caret-color: transparent !important;
+  }
   html {
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
